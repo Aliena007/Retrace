4eb59eae6cdfd2f70bba98679d05be4862964094
Tanish Fitkariwala <74542494+Agent0075430V@users.noreply.github.com>
2025-10-06 22:32:15 +0530

    Add files via upload

---
+++ Product/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .api_views import LostProductViewSet, FoundProductViewSet, MatchResultViewSet, NotificationViewSet, RouteMapViewSet
+
+router = DefaultRouter()
+router.register(r'lost', LostProductViewSet, basename='lost')
+router.register(r'found', FoundProductViewSet, basename='found')
+router.register(r'matches', MatchResultViewSet, basename='matches')
+router.register(r'notifications', NotificationViewSet, basename='notifications')
+router.register(r'routes', RouteMapViewSet, basename='routes')
+
+urlpatterns = [
+    path('api/ai/', include(router.urls)),
+]
